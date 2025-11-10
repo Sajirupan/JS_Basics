95d6e97820d9d7d878da539bbdeb389f995bb8fb
Sajirupan <balabavansajirupan@gmail.com>
2025-11-10 12:02:48 +0530

    2025-11-10

--- script.js
+++ script.js
@@ -32,6 +32,33 @@
 // console.log(typeof age);
 
 
+// 2025-11-10
+
+
+// Hoisting
+console.log(name);
+var name="saji";
+console.log(name);
+const name="saji";
+console.log(name);
+let name="saji";
+
+// Scope
+{
+    const x=25;
+    console.log(x);
+}
+console.log(x);
+{
+    let x=25;
+    console.log(x);
+}
+console.log(x);
+{
+    var x=25;
+    console.log(x);
+}
+console.log(x);
 
 console.log(Nuw);
 var Nuw = 25;

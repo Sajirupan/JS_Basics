7a2b87798bac9cbfa44e9fb7f4d2115472d4059e
Sajirupan <balabavansajirupan@gmail.com>
2025-11-10 11:43:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// console.log("Sajirupan");
+// console.log("Puttur");
+// console.log("Age-21");
+// console.log("Trip");
+
+
+
+// console.log("5+7");
+// document.getElementById("text").innerHTML="Hello World";
+// document.write("This is a popular Web page");
+// window.alert("You are a wrong member");
+// console.log("Java Script is fun");
+// const name="Saji";
+// var age="21";
+// let subject="Maths";
+// console.log(name);
+// console.log(age);
+// console.log(subject);
+// var favcolor =prompt("inout your favorite color");
+// console.log(favcolor);
+// document.body.style.backgroundColor = favcolor;
+// let price = 500;
+// let quanity =150;
+// let display = price * quanity;
+// console.log(display);
+// let color;
+// console.log(color);
+// let firstname = "Saji";
+// let lastname = "Rupan";
+// console.log("My name is"+" "+firstname +" "+ lastname);
+// let age = "21" ;
+// console.log(typeof age);
+
+
+
+console.log(Nuw);
+var Nuw = 25;
+
+{var x = 25;
+    console.log(x)
+}
+ console.log(x)
+

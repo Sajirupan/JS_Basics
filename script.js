bdd41c5c95ba612c84a625ea8ab5956517386691
Sajirupan <balabavansajirupan@gmail.com>
2025-11-11 22:23:59 +0530

    11-11-2025

--- script.js
+++ script.js
@@ -32,39 +32,148 @@
 // console.log(typeof age);
 
 
-// 2025-11-10
+// // 2025-11-10
 
 
-// Hoisting
-console.log(name);
-var name="saji";
-console.log(name);
-const name="saji";
-console.log(name);
-let name="saji";
+// // Hoisting
+// console.log(name);
+// var name="saji";
+// console.log(name);
+// const name="saji";
+// console.log(name);
+// let name="saji";
 
-// Scope
-{
-    const x=25;
-    console.log(x);
-}
-console.log(x);
-{
-    let x=25;
-    console.log(x);
-}
-console.log(x);
-{
-    var x=25;
-    console.log(x);
-}
-console.log(x);
+// // Scope
+// {
+//     const x=25;
+//     console.log(x);
+// }
+// console.log(x);
+// {
+//     let x=25;
+//     console.log(x);
+// }
+// console.log(x);
+// {
+//     var x=25;
+//     console.log(x);
+// }
+// console.log(x);
 
-console.log(Nuw);
-var Nuw = 25;
+// console.log(Nuw);
+// var Nuw = 25;
 
-{var x = 25;
-    console.log(x)
-}
- console.log(x)
+// {var x = 25;
+//     console.log(x)
+// }
+//  console.log(x)
 
+
+
+// 2025-11-11
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_VALUE*2);
+// console.log(Number.MIN_VALUE/2);
+// console.log(Number.EPSILON);
+// console.log("Number.MIN_VALUE");
+// console.log(5e-4);
+// console.log(5e+4);
+// console.log(Number.string/16);
+// // let.z=56.789
+// // console.log(z.toString(2));
+// // let.distance=12.3456;
+// // console.log(distance.toFixed(3));
+// let name="Saji";
+// let l_name="Rupan";
+// x=name.toUpperCase();
+// console.log(x);
+// console.log(l_name.toLowerCase());
+// console.log(name.charAt(2));
+// console.log(`Welcome to UKI, ${name}`);
+// console.log(name.lenght);
+// console.log(l_name.slice(1,4));
+// console.log(l_name.substring(1,4));
+// console.log(name.replace("S","Z"));
+// console.log(l_name.replaceAll());
+// let f_name="       Sajirupan      ";
+// console.log(f_name.trimStart());
+// console.log(f_name.trimEnd());
+// console.log(f_name.trim())
+// let text="Kapi,Roni,b,c,d,e,f";
+// const myArray = text.split("");
+// console.log(myArray[1]);
+// let fullname=name.concat(" ",l_name);
+// console.log(fullname);
+// let textt ="5";
+// console.log(textt.padStart(4,"0"));
+// console.log(textt.padEnd(4,"0"));
+// // let x;
+// // console.log(Boolean(x));
+// // let y=10;
+// // console.log(Boolean(y));
+// // let z=0;
+// // console.log(Boolean(z));
+
+// let e=5;
+// let f=10;
+// console.log(Boolean(e>f));
+// console.log(Boolean(e<f));
+// let Name=true;
+// if(Name){
+//     console.log("Your name is Kapi")
+// }
+// else{
+//     console.log("Invalide Name")
+// }
+// let num1 =15;
+// let num2 =20;
+// if(num1>num2){
+//     console.log("correct")
+// }
+// else{
+//     console.log("wrong")
+// }
+// let x1=false;
+// let y1=new Boolean(false);
+// console.log(x1==y1);
+// console.log(x1===y1);
+
+// let Customer_Name= prompt("Enter your name:");
+// let Prices=Number( prompt("Enter the product price"));
+// let discountPercent = 17.33;
+// let discountAmount = (price * discountPercent)/100;
+// console.log("Recepit");
+// console.log("Customer "+ Customer_Name.toUpperCase());
+// console.log("Original Price: LKR "+ Prices.toFixed(2));
+// console.log("Discount (" + discountPercent + "%): LKR " + discountAmount.toFixed(2));
+// console.log("Total after discount: LKR " + TOTAL.toFixed(2));
+// alert(
+//   "Hello " + customerName + "!\n" +
+//   "Your total after " + discountPercent + "% discount is LKR " + TOTAL.toFixed(2)
+// );
+
+// Get user input
+let customerName = prompt("Enter your name:");
+let price = Number(prompt("Enter the product price:"));
+
+// Fixed discount percentage
+let discountPercent = 17.33; // 17.33% discount
+
+// Calculate discount amount and total
+let discountAmount = (price * discountPercent) / 100;
+let TOTAL = price - discountAmount;
+
+// Display receipt in console
+console.log("🧾 Receipt");
+console.log("Customer: " + customerName.toUpperCase());
+console.log("Original Price: LKR " + price.toFixed(2));
+console.log("Discount (" + discountPercent + "%): LKR " + discountAmount.toFixed(2));
+console.log("Total after discount: LKR " + TOTAL.toFixed(2));
+
+// Show final message.p -
+alert(
+  "Hello " + customerName + "!\n" +
+  "Your total after " + discountPercent + "% discount is LKR " + TOTAL.toFixed(2)
+);
